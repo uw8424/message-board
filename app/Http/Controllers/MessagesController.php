b90f67e33ad51f9c8a86a79aba57d6c74335839d
naoya.kato <xariarl84@gmail.com>
2021-11-17 16:50:49 +0000

    validation

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -44,6 +44,11 @@ class MessagesController extends Controller
      */
     public function store(Request $request)
     {
+        
+        $request->validate([
+            "content" => "required|max:255",
+            ]);
+            
         $message = new Message;
         $message->content = $request->content;
         $message->save();
@@ -90,6 +95,10 @@ class MessagesController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            "content" => "required|max:255",
+            ]);
+        
         $message = Message::findOrFail($id);
         
         $message->content = $request->content;
