b7d17b2feed3c18b47c49fa3867d7307540eca1a
naoya.kato <xariarl84@gmail.com>
2021-11-18 08:55:33 +0000

    kadai

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -15,7 +15,7 @@ class MessagesController extends Controller
      */
     public function index()
     {
-        $messages = Message::orderBy("id", "desc")->paginate(25);
+        $messages = Message::orderBy("id")->paginate(25);
         
         return view("messages.index",[
             "messages" => $messages,
--- database/seeds/MessagesTableSeeder.php
+++ database/seeds/MessagesTableSeeder.php
@@ -11,12 +11,6 @@ class MessagesTableSeeder extends Seeder
      */
     public function run()
     {
-    for($i = 1; $i <= 100; $i++) {
-        DB::table("messages")->insert([
-            "title" => "test title" . $i,
-            "content" => "test content" . $i,
-            ]);
-    }
 
         }
     }
